a4fd5a258a4f6c39822c7438718bf069c7bdb727
Yoshinari Gyu <50069930+yngyu@users.noreply.github.com>
2024-04-12 02:17:24 +0900

    add recording

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::{env, error::Error};
 
+use chrono::DateTime;
 use reqwest::{
     self,
     header::{HeaderMap, AUTHORIZATION},
 };
 
 const NATURE_API_ENDPOINT: &str = "https://api.nature.global/1/devices";
-const PLACE: &str = "リビング";
+const PLACE_JA: &str = "リビング";
+const PLACE_TAG: &str = "Living";
+const SENSOR_TAG: &str = "Nature-Remo-mini-2";
 
 fn main() -> Result<(), Box<(dyn Error + 'static)>> {
     env_logger::init();
 
-    let token = env::var("NATURE_TOKEN").expect("Could not get environment variable NATURE_TOKEN.");
-    let authorization = format!("Bearer {}", token);
+    let nature_token =
+        env::var("NATURE_TOKEN").expect("Could not get environment variable NATURE_TOKEN.");
+    let influxdb_host =
+        env::var("INFLUXDB_HOST").expect("Could not get environment variable INFLUXDB_HOST.");
+    let influxdb_token =
+        env::var("INFLUXDB_TOKEN").expect("Could not get environment variable INFLUXDB_TOKEN.");
+    let influxdb_org =
+        env::var("INFLUXDB_ORG").expect("Could not get environment variable INFLUXDB_ORG.");
+    let influxdb_bucket =
+        env::var("INFLUXDB_BUCKET").expect("Could not get environment variable INFLUXDB_BUCKET.");
+
+    let authorization = format!("Bearer {}", nature_token);
     let mut headers = HeaderMap::new();
     headers.insert(AUTHORIZATION, authorization.parse().unwrap());
 
@@ -28,7 +41,7 @@ fn main() -> Result<(), Box<(dyn Error + 'static)>> {
         let place = device_info["name"]
             .as_str()
             .expect("respond must have name field");
-        if place != PLACE {
+        if place != PLACE_JA {
             continue;
         }
 
@@ -39,10 +52,32 @@ fn main() -> Result<(), Box<(dyn Error + 'static)>> {
         break;
     }
 
-    let time = event["created_at"].as_str().expect("there must be created_at field");
+    let time = event["created_at"]
+        .as_str()
+        .expect("there must be created_at field");
+    let unixtime = DateTime::parse_from_rfc3339(time)
+        .expect("should be rfc3339 str")
+        .timestamp();
     let temperature = event["val"].as_f64().expect("there must be val field");
 
-    dbg!(&time, &temperature);
+    let influxdb_end_point = format!(
+        "{}/api/v2/write?org={}&bucket={}",
+        influxdb_host, influxdb_org, influxdb_bucket
+    );
+    let post_field = format!(
+        "{},sensor_id={} temperature={} {}000000000",
+        PLACE_TAG, SENSOR_TAG, temperature, unixtime
+    );
+
+    let authorization = format!("Token {}", influxdb_token);
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, authorization.parse().unwrap());
+
+    let client = reqwest::blocking::Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    client.post(influxdb_end_point).body(post_field).send()?;
 
     Ok(())
 }
